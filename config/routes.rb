3f4d8956dd7fd57b056fd2dfb246698fb8acc5c0
Ann Murray Dunning <ann@amurraypr.com>
2018-12-09 15:18:22 -0800

    Creating signup and signout elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+
+# new prefixes
+  as :user do
+    get "signin" => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get 'signup' => 'devise/registrations#new'
+  end
+
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'add' => 'pages#add'
   get 'edit' => 'pages#edit'
   get 'show' => 'pages#show'
-  get 'signup' => 'pages#signup' #here is where users sign up 
+  get 'signup' => 'pages#signup' #here is where users sign up
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
