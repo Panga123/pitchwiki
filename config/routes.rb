0bba295f6a50ae34282c30ddc48fe3ae3912000b
Ann Murray Dunning <ann@amurraypr.com>
2018-12-09 13:49:56 -0800

    User Sign Up Page Completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'add' => 'pages#add'
   get 'edit' => 'pages#edit'
   get 'show' => 'pages#show'
-  get 'signup' => 'pages#signup'
+  get 'signup' => 'pages#signup' #here is where users sign up 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
